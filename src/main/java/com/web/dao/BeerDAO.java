a0b840a361906b8a23c8a78b8e19c8326f28cedf
JakubWitczak <jakubwitczak6@gmail.com>
2016-12-18 20:27:16 -0800

    added interface for hamdle database operation
    using JpaRepository

---
+++ src/main/java/com/web/dao/BeerDAO.java
@@ -0,0 +1,13 @@
+package com.web.dao;
+
+import com.web.model.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by JakubWitczak on 18.12.2016.
+ */
+@Repository
+public interface BeerDAO extends JpaRepository<Beer, Long>{
+    Beer findByIndexNumber(String indexNumber);
+}
